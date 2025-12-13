a0bb535c0211d1847d9b1c954f2291e6be4e954f
Sami Kallinen <sami@kpsystem.se>
2025-12-13 20:59:26 +0200

    Revert 9p mount to fix boot hang

--- vm-configuration.nix
+++ vm-configuration.nix
@@ -28,12 +28,6 @@
     options = [ "fmask=0022" "dmask=0022" ];
   };
 
-  fileSystems."/home/sakalli/common" = {
-    device = "common";
-    fsType = "9p";
-    options = [ "trans=virtio" "version=9p2000.L" "cache=loose" ];
-  };
-
   # --- Networking ---
   networking.hostName = "nixos-vm";
   networking.networkmanager.enable = true;
